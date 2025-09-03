7cc4d05246c0bb588dbd63c32017bd699410af07
FyreHyer <afifb130@example.com>
2025-09-03 17:07:06 +0800

    Update llamachatbot.py

--- llamachatbot.py
+++ llamachatbot.py
@@ -1,22 +1,68 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from langchain.document_loaders import PyPDFLoader 
+from vector import retriever
 
 model = OllamaLLM(model="llama3.2")
 
 template = """
-You are an expert of providing solutions for software and hardware problems and provide information regarding ROHM company
-Here are some solutions: {solutions}
-Here are some information:{information}
+You are an expert in providing solutions for software and hardware problems, but you can also answer other questions. Use the following context if it is relevant: {solutions}
 
-Here is the question to the answer: {question}
+If the context is not related to the question, ignore it and answer naturally or say there is no information available.
+
+Answer the question: {question}
+Do not repeat answers. If the user wants to talk about something else, change the topic.
+If all solutions have been used and none worked, tell the user to contact ISD support.
 """
 
 prompt = ChatPromptTemplate.from_template(template)
 chain  = prompt | model
 
-result = chain.invoke({"solutions": [],"information": [], "question": "Who is the president of ROHM?"})
+print("---------------------------------------------------------------------")
+question = input("Enter your question (type 'q' to quit): ")
+print("---------------------------------------------------------------------")
+if question.lower() == 'q':
+    print("Exiting chatbot.")
+
+ # Retrieve context and get answer
+docs = retriever.get_relevant_documents(question)
+solutions = "\n\n".join([doc.page_content for doc in docs])
+
+print("\n--- Retrieved Context ---\n")
+print(solutions)
+print("\n--- End of Context ---\n")
+
+result = chain.invoke({"solutions": solutions, "question": question})
+print(result)
+
+while True:
+    
+
+    print("---------------------------------------------------------------------")
+    question = input("Enter your question (type 'q' to quit): ")
+    print("---------------------------------------------------------------------")
+    if question.lower() == 'q':
+        print("Exiting chatbot.")
+        break
+    docs = retriever.get_relevant_documents(question)
+    solutions = "\n\n".join([doc.page_content for doc in docs])
+
+    print("\n--- Retrieved Context ---\n")
+    print(solutions)
+    print("\n--- End of Context ---\n")
+
+    result = chain.invoke({"solutions": solutions,"question": question})
+    print(result)
+
 
 print(result)
+
+
+
+
+
+
+
 
 
 
